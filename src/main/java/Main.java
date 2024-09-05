83278061e3b5cea5197d15ff5776996bb4caef22
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-09-05 10:54:59 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+   int contador = 1;
+   String nome = "Beatriz";
+    while(contador < 11) {
+      
+      System.out.println("Bom dia!");
+      contador = contador + 1;
+    }
+  }
+
+}
